18108fd532ddb1c1cb1700054f9d8a6d70579f34
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2024-12-01 22:20:14 +0200

    Super starter for p2

--- p2.py
+++ p2.py
@@ -1,2 +1,15 @@
+row1 = []
+row2 = []
+
 with open('puzzleInput.txt', 'r') as file:
     lines = file.readlines()
+
+    for line in lines:
+        firstRower = int(line.split("   ")[0])
+        secondRower = int(line.split("   ")[1])
+
+        row1.append(firstRower)
+        row2.append(secondRower)
+
+row1.sort()
+row2.sort()
