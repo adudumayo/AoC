cd612c82ecb446877df1bf3b0c6f553ed14bc584
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2024-12-01 21:09:49 +0200

    Lists sorted ready for disaster

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print("Welcome to day one")
+row1 = []
+row2 = []
 
+with open('puzzleInput.txt', 'r') as file:
+    lines = file.readlines()
+
+    for line in lines:
+        firstRower = int(line.split("   ")[0])
+        secondRower = int(line.split("   ")[1])
+
+        row1.append(firstRower)
+        row2.append(secondRower)
+
+row1.sort()
+row2.sort()
+
+print(row1)
+print()
+print(row2)
