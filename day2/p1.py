9e54c9bac88fddbdd4de2c0b89d6ca4b0654a762
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2024-12-02 23:20:24 +0200

    Logic Trusted, Need To Be Tested

--- day2/p1.py
+++ day2/p1.py
@@ -1,17 +1,54 @@
 import time
 
-# Start the timer
+safeCount = 0
+levels = []
+loopBreaked = False
+
 start_time = time.time()
 
-# Code block to be timed
-# Example: a simple loop
-for i in range(1000000):
-    pass
+def listIsAscending(lst):
+    for i in range(len(lst) - 1):
+        if lst[i] > lst[i + 1]:
+            return False
+    return True
 
-# End the timer
+def listIsDescending(lst):
+    for i in range(len(lst) - 1):
+        if lst[i] < lst[i + 1]:
+            return False
+    return True
+
+def listIsASet(lst):
+    return len(lst) == len(set(lst))
+
+with open('puzzleInput.txt', 'r') as file:
+    lines = file.readlines()
+
+for line in lines:
+    values = line.split(" ")
+
+    for i in range(len(values)):
+        levels.append(int(values[i]))
+
+    if listIsAscending(levels) or listIsDescending(levels) and listIsASet(levels):
+        print(levels)
+        for i in range(len(levels)-1):
+            diff = abs(levels[i] - levels[i+1])
+
+            if (diff < 1 or diff > 3):
+                loopBreaked = True
+                break
+
+        if loopBreaked:
+            safeCount += 1
+            loopBreaked = False
+
+    levels = []
+
+print(safeCount)
+
 end_time = time.time()
 
-# Calculate the elapsed time
 elapsed_time = end_time - start_time
 
 print(f"Elapsed time: {elapsed_time} seconds")
